2dc3d748bc0698ae5962e056c13ef684dcf7bf6e
pdmay <pdmay@DESKTOP-CS1UFF9>
2020-06-07 17:46:01 -0300

    Cria a classe CategoriaAtividade
    Além dos atributos ela foi encapsulada

---
+++ src/pooprojetointegrador3/CategoriaAtividade.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pooprojetointegrador3;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author pdmay
+ */
+public class CategoriaAtividade {
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 89 * hash + this.Codigo;
+        hash = 89 * hash + Objects.hashCode(this.Nome);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final CategoriaAtividade other = (CategoriaAtividade) obj;
+        if (this.Codigo != other.Codigo) {
+            return false;
+        }
+        if (!Objects.equals(this.Nome, other.Nome)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "CategoriaAtividade{" + "Codigo=" + Codigo + ", Nome=" + Nome + '}';
+    }
+    private int Codigo;
+    private String Nome;
+
+    /**
+     * @return the Codigo
+     */
+    public int getCodigo() {
+        return Codigo;
+    }
+
+    /**
+     * @param Codigo the Codigo to set
+     */
+    public void setCodigo(int Codigo) {
+        this.Codigo = Codigo;
+    }
+
+    /**
+     * @return the Nome
+     */
+    public String getNome() {
+        return Nome;
+    }
+
+    /**
+     * @param Nome the Nome to set
+     */
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+    
+}
